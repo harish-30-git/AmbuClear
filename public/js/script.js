695dfdb3881d0c8b32f7abc67f43d13677737562
harish-30-git <srihariprakash1112@gmail.com>
2025-04-12 19:26:00 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -7,7 +7,7 @@ if (navigator.geolocation) {
             socket.emit("send-location", { latitude, longitude });
 
             let closestLocation = null;
-            let minDistance = 500;
+            let minDistance = 100000;
 
             highlightLocations.forEach((location) => {
                 const distance = getDistanceFromLatLonInMeters(
